ff69e8f197a5636a83cad2ce0561b0b94d2280bd
Christoph Sitter <christoph.sitter@caplin.com>
2015-09-30 17:18:48 +0100

    implement termios based client - completion only suggests and arrow keys (history) is not implemented yet

--- src/main.rs
+++ src/main.rs
@@ -4,95 +4,166 @@ extern crate termios;
 use std::io;
 use std::io::prelude::*;
 use std::os::unix::io::RawFd;
+use cline::Cli;
 use termios::*;
 
-fn main() {
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+#[derive(Debug)]
+enum Key {
+    Char(char),
+    Symbol(char),
+    Digit(u8),
+    Arrow(Direction),
+    Whitespace,
+    Backspace,
+    Del,
+    Tab,
+    Newline,
+    Etx
+}
+
+fn read_key<T: Read>(bytes: &mut std::io::Bytes<T>) -> Option<Key> {
+    match bytes.next() {
+        Some(Ok(0x1B)) => { //Esc
+            match bytes.next() {
+                Some(Ok(0x5b)) => { // [
+                    match bytes.next() {
+                        Some(Ok(0x41)) => { // A
+                            Some(Key::Arrow(Direction::Up))
+                        },
+                        Some(Ok(0x42)) => { // B
+                            Some(Key::Arrow(Direction::Down))
+                        },
+                        Some(Ok(0x43)) => { // C
+                            Some(Key::Arrow(Direction::Right))
+                        },
+                        Some(Ok(0x44)) => { // D
+                            Some(Key::Arrow(Direction::Left))
+                        },
+                        c @ _ => {
+                            println!("{} {} {}", 0x1B, 0x5b, c.unwrap().unwrap());
+                            None
+                        }
+                    }
+                },
+                c @ _ => {
+                    println!("{} {}", 0x1B, c.unwrap().unwrap());
+                    None
+                }
+            }
+        },
+        Some(Ok(c)) if c >= 0x30 && c <= 0x39 => {
+            Some(Key::Digit(c - 0x30))
+        },
+        Some(Ok(c)) if (c >= 0x41 && c <= 0x5A) || (c >= 0x61 && c <= 0x7A) => {
+            Some(Key::Char(c as char))
+        },
+        Some(Ok(0x20)) => {
+            Some(Key::Whitespace)
+        },
+        Some(Ok(0x7f)) => {
+            Some(Key::Del)
+        },
+        Some(Ok(0x09)) => {
+            Some(Key::Tab)
+        },
+        Some(Ok(0x08)) => {
+            Some(Key::Backspace)
+        },
+        Some(Ok(0xA)) => {
+            Some(Key::Newline)
+        },
+        Some(Ok(0x3)) => {
+            Some(Key::Etx)
+        },
+        Some(Ok(c)) => {
+            println!("{:?}", c);
+            Some(Key::Symbol(c as char))
+        },
+        _ => {
+            None
+        }
+    }
+}
+
+
+fn run(cli: &mut Cli) {
     let mut termios = Termios::from_fd(0).unwrap();
     let term_orig = termios;
-    let mut cli = cline::Cli::new();
-    let mut buf:Vec<u8> = Vec::new();
+    let mut input_iter = io::stdin().bytes();
+    let mut command = String::new();
 
-    cli.register(vec!["show", "stuff"], | _ | { println!("wooo haaa") });
-    cli.register(vec!["show", "other"], | _ | { println!("wooo haaa") });
-    cli.register(vec!["foo", "bar"], | _ | { println!("wooo haaa") });
-    cli.register(vec!["exit"], | _ | { 
-        tcsetattr(0, termios::TCSANOW, &term_orig);
-        std::process::exit(0); 
-    });
-   
-
-
-    //termios.c_lflag = ECHONL;
     termios.c_lflag &= !(ICANON | IEXTEN | ISIG | ECHO);
     tcsetattr(0, TCSANOW, &termios);
     tcflush(0, TCIOFLUSH);
 
-    println!("-----> {:?}", term_orig);
     print!(">> ");
     io::stdout().flush();
-    for byte in io::stdin().bytes() {
-        let b = byte.unwrap();
-        let mut command:String = String::new();
-        if let Ok(string) = String::from_utf8(buf.clone()) {
-            command = string;
-        }
 
-
-        match b {
-            3 => break,
-            9 => {
-                //Tab
-                let mut outbuf:String = String::new();
-                let res = cli.complete(&command);
-                
-                for suggestion in res.iter() {
-                    outbuf.push_str(suggestion);
-                    outbuf.push_str("  ");
-                }
-
-                //TODO: meh...
-                match command.chars().last() {
-                    Some(' ') => {
-                        if res.len() == 1 {
-                            buf.extend(res[0].bytes());
-                            command.push_str(res[0]);
-                        }
+    loop {
+        match(read_key(&mut input_iter)) {
+            Some(key) => {
+                match key {
+                    Key::Char(c) => {
+                        command.push(c);
+                        print!("{}", c);
+                        io::stdout().flush();
+                    },
+                    Key::Whitespace => {
+                        command.push(' ');
+                        print!(" ");
+                        io::stdout().flush();
                     }
-                    _ => {
-                        if res.len() > 1 {
-                        } else if res.len() == 1 {
-                        } else {
-                            //nothing found
+                    Key::Del | Key::Backspace => {
+                        command.pop();
+                        print!("{} {}", 0x08 as char, 0x08 as char);
+                        io::stdout().flush();
+                    },
+                    Key::Tab => {
+                        let suggestions = cli.complete(&command);
+                        print!("{}", '\n');
+                        for cmd in suggestions.iter() {
+                            print!("{} ", cmd);
                         }
+                        print!("\n>> {}", command);
+                        io::stdout().flush();
+                    },
+                    Key::Newline => {
+                        println!("");
+                        cli.exec(&command);
+                        command.clear();
+                        print!(">> ");
+                        io::stdout().flush();
+                    },
+                    Key::Etx => { //Ctrl + C
+                        println!("");
+                        break;
+                    }
+                    x @ _ => {
+                        println!("unhandled: {:?}", x);
                     }
                 }
-       
-                println!("\n{}", outbuf);
-                print!(">> {}", command);
-                io::stdout().flush();
-            }
-            10 => {
-                //CRLF
-                println!("\nexecute for: '{}'", command);
-                cli.exec(&command);
-                command.clear();
-                buf.clear();
-                print!(">> ");
-                io::stdout().flush();
-            }
-            0x7f => {
-                //backspace
-                print!("{} {}", 0x08 as char, 0x08 as char);
-                io::stdout().flush();
-                buf.pop();
-            }
-            _ => {
-                buf.push(b);
-                print!("{}", b as char);
-                io::stdout().flush();
+            },
+            None => {
+                break;
             }
         }
     }
-
     tcsetattr(0, termios::TCSANOW, &term_orig);
+}
+
+fn main() {
+    let mut cli = Cli::new();
+
+    cli.register(vec!["foo", "bar"], | _ | { println!("running foo bar") });
+    cli.register(vec!["foo", "baz"], | _ | { println!("running foo baz") });
+
+    run(&mut cli);
 }
